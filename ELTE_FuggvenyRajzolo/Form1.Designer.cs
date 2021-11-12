18e206b9e6bcaeea1930d60059961a9ac3f0d690
MolnAtt <molnar.attila@szlgbp.hu>
2021-11-12 17:23:11 +0100

    Add project files.

---
+++ ELTE_FuggvenyRajzolo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ELTE_FuggvenyRajzolo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vaszon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.vaszon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vaszon
+            // 
+            this.vaszon.BackColor = System.Drawing.SystemColors.Desktop;
+            this.vaszon.Location = new System.Drawing.Point(12, 12);
+            this.vaszon.Name = "vaszon";
+            this.vaszon.Size = new System.Drawing.Size(776, 426);
+            this.vaszon.TabIndex = 0;
+            this.vaszon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vaszon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vaszon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox vaszon;
+    }
+}
+
---
+++ ELTE_FuggvenyRajzolo/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ELTE_FuggvenyRajzolo
+{
+    public partial class Form1 : Form
+    {
+        void FvRajz(int xa, int xf, Func<double, double> f)
+        { 
+
+        }
+
+        void Főprogram(PictureBox vászon)
+        {
+            double xa = 0;
+            double xf = 3 * Math.PI;
+            int xh = vászon.Size.Width;
+            Func<double, double> f = x => Math.Sin(x);
+            double[] fv = Számítás(xa,xf,xh, f);
+            (double ya, double yf) = Szélsőérték(xa, xf, xh, fv);
+            Normálás(ya, yf,xh, fv);
+            Rajzolás(vászon, fv);
+            /*
+            */
+
+        }
+
+        double[] Számítás(double xa, double xf, int xh, Func<double, double> f)
+        {
+            double hol = xa;
+            int j = 0;
+            double lepx = (xf - xa) / xh;
+            double[] fv = new double[xh+1];
+            while (hol <= xf)
+            {
+                fv[j] = f(hol);
+                hol += lepx;
+                j++;
+            }
+            return fv;
+        }
+        (double, double) Szélsőérték(double xa, double xf, double xh, double[] fv)
+        {
+            double ya = fv[0];
+            double yf = fv[0];
+            for (int i = 1; i < xh; i++)
+            {
+                if (ya > fv[i]) ya = fv[i];
+                if (yf < fv[i]) yf = fv[i];
+            }
+            return (ya, yf);
+        }
+        void Normálás(double ya, double yf, int xh, double[] fv)
+        {
+            double norm = Math.Abs(yf - ya);
+            for (int i = 0; i < xh; i++)
+                fv[i] /= norm;
+        }
+
+        void Rajzolás(PictureBox vászon, double[] fv)
+        {
+            int ox = 0; // hol az origo vízszintesen
+            int yh = vászon.Size.Height;
+            int xh = vászon.Size.Width;
+            int oy = yh/2; // hol az origó függőlegesen
+            Bitmap kep = new Bitmap(xh,yh);
+            for (int i = 0; i < xh; i++)
+            {
+                int xp = ox + i;
+                int yp = (int)Math.Round((double)oy - (double)yh * fv[i]);
+                Pont(xp, yp, kep);
+            }
+            vászon.Image = kep;
+            vászon.Refresh();
+
+        }
+
+        void Pont(int xp, int yp, Bitmap kep)
+        {
+            if (yp<kep.Height)
+            {
+                kep.SetPixel(xp, yp, Color.White);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Főprogram(vaszon);
+        }
+    }
+}
